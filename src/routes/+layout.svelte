07144828240b246503594b94288a524feb5be1de
MatiasSagredo <ma.sagredom@duocuc.cl>
2026-05-26 15:35:07 -0400

    feat: agregar metadatos y título a la cabecera del layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,5 +5,13 @@
     let { children } = $props();
 </script>
 
+<svelte:head>
+    <title>anti-infernum - Gestión de Incendios</title>
+    <meta
+        name="description"
+        content="Plataforma inteligente para la prevención de desastres."
+    />
+</svelte:head>
+
 <Navbar />
 {@render children()}
